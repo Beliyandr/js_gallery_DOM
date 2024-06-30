4b0faa4a8fe49096127ca63bd5049b8edd4c9cb6
beliyandr <beliyandr@gmail.com>
2024-06-30 19:05:05 +0300

    make build

---
+++ index.376e3eed.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),t=document.getElementById("largeImg");e.querySelector(".gallery__list").addEventListener("click",function(e){e.preventDefault(),t.src=e.target.closest(".list-item__link").href});
+//# sourceMappingURL=index.376e3eed.js.map
